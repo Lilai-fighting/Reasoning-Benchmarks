75fed55d7212866bd22835684bd303a4afe94dc6
Lilai Liu <lilai.liu22@imperial.ac.uk>
2026-04-30 17:12:50 +0100

    Add benchmark Geometry3K

---
+++ Geometry3K/README.md
@@ -0,0 +1,5 @@
+unzip data files into `Geometry3K/`:
+
+```shell
+. Geometry3K/unzip_data.sh
+```
---
+++ Geometry3K/unzip_data.sh
@@ -0,0 +1,5 @@
+unzip -d Geometry3K Geometry3K/train.zip
+unzip -d Geometry3K Geometry3K/val.zip
+unzip -d Geometry3K Geometry3K/test.zip
+unzip -d Geometry3K Geometry3K/logic_forms.zip
+unzip -d Geometry3K Geometry3K/symbols.zip
