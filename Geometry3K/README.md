cbac3104f29c4bca4482a4f1bfcf88dc811357b8
Lilai Liu <lilai.liu22@imperial.ac.uk>
2026-06-24 22:24:31 +0100

    remove unused data

--- Geometry3K/README.md
+++
@@ -1,5 +0,0 @@
-unzip data files into `Geometry3K/`:
-
-```shell
-. Geometry3K/unzip_data.sh
-```
--- Geometry3K/unzip_data.sh
+++
@@ -1,5 +0,0 @@
-unzip -d Geometry3K Geometry3K/train.zip
-unzip -d Geometry3K Geometry3K/val.zip
-unzip -d Geometry3K Geometry3K/test.zip
-unzip -d Geometry3K Geometry3K/logic_forms.zip
-unzip -d Geometry3K Geometry3K/symbols.zip
